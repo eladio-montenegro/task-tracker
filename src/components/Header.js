34875ffb2785cd385a2be41861fe4bd0ce0b6f6f
Eladio Montenegro <eladio.montenegro@gmail.com>
2022-07-15 17:30:49 -0500

    removing the Add button from the About page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
 
-const Header = ({ title, onAdd, showAdd }) => {    
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
     return (
 <header className='header'>
     <h1>{title}</h1>
-    <Button 
+    {location.pathname === '/' && (<Button 
     color={showAdd ? 'red' : 'green'} 
     text={showAdd ? 'Close' : 'Add'} 
     onClick={onAdd} 
-    />
+    />)}
 </header>    
     )
 }
